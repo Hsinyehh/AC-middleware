ac6ef999beca0d51c73dc668d0e954ee18b44099
Rita <hsen.yeh123@gmail.com>
2021-05-22 22:15:57 +0800

    feat: add responseTime

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 // app.js
+const { response } = require('express')
 const express = require('express')
+const { duration } = require('moment')
 const moment = require('moment')
 const app = express()
 const port = 3000
 
+
+
+
 app.use(function (req, res, next) {
   const currentDate = new Date()
   const TimeStamp = currentDate.getTime()
-  console.log(moment(TimeStamp).format('YYYY-MM-DD HH:mm:ss') + ' | ' + req.method + ' from ' + req.originalUrl)
+  req.start = Date.now()
+
+  console.log('requestTime: ' + moment(req.start).format('YYYY-MM-DD HH:mm:ss.SSS'))
+  res.on('finish', function () {
+    const responseTime = Date.now()
+    console.log('responseTime: ' + moment(responseTime).format('YYYY-MM-DD HH:mm:ss.SSS'))
+    const duration = responseTime - req.start
+
+    console.log(moment(req.start).format('YYYY-MM-DD HH:mm:ss.SSS') + ' | ' + req.method + ' from ' + req.originalUrl + ' | Total Time: ' + duration + 'ms')
+  })
+
   next()
 })
 
@@ -30,4 +45,9 @@ app.post('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`)
+})
+
+app.use(function (req, res) {
+  const time = Date.now() - req.start
+  console.log(time)
 })
